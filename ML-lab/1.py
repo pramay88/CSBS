39c654d4838b3be37d49dec2f92a23b662f7fba5
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2025-09-18 11:02:38 +0530

    Update 1.py

--- ML-lab/1.py
+++ ML-lab/1.py
@@ -1,1 +1,56 @@
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+import seaborn as sns
+import matplotlib.pyplot as plt
+# Loading and Preparing the Iris Dataset
+iris = load_iris()
+iris_df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+iris_df['Species'] = iris.target
+X = iris_df.drop('Species', axis=1)
+y = iris_df['Species']
+display(iris_df.head())
 
+# Splitting the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+# Feature Scaling
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = {
+    "K-Nearest Neighbors": KNeighborsClassifier(n_neighbors=3),
+    "Gaussian Naive Bayes": GaussianNB(),
+    "Support Vector Machine": SVC(),
+    "Decision Tree": DecisionTreeClassifier()
+}
+
+target_names = y.unique().astype(str)
+
+for name, current_model in model.items():
+    current_model.fit(X_train, y_train)     # Train the model
+    y_pred = current_model.predict(X_test)  # Make predictions
+    acc = accuracy_score(y_test, y_pred)
+    cm = confusion_matrix(y_test, y_pred)
+
+    print(f"\nModel Name: {name}")
+    print("Accuracy:", round(acc*100, 4))
+    print("Classification Report:\n", classification_report(y_test, y_pred, target_names=target_names))
+
+    plt.figure(figsize=(4, 3))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=target_names, yticklabels=target_names)
+    plt.title(f"Confusion Matrix - {name}")
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.tight_layout()
+    plt.show()
+  
